53bb339785eabdb1e1333189e0f5bf14e563a945
Vanessa Barbosa <vanessabmdf@gmail.com>
2014-04-03 14:59:06 -0300

    Configurando as dependencias do Mysql

--- RoboVelc/grails-app/controllers/robovelc/RoboController.groovy
+++ RoboVelc/grails-app/controllers/robovelc/RoboController.groovy
@@ -6,23 +6,9 @@ import grails.transaction.Transactional
 @Transactional(readOnly = true)
 class RoboController {
 
-    def FatorKService, FatorMService, FatorHService
+    def scaffold = Robo
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
-
-    def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        respond Robo.list(params), model:[roboInstanceCount: Robo.count()]
-    }
-
-    def show(Robo roboInstance) {
-        respond roboInstance
-    }
-
-    def create() {
-        respond new Robo(params)
-    }
-
+    def FatorKService, FatorMService, FatorHService  
 
     def selecaoCorrelacao(){
 
@@ -39,8 +25,7 @@ class RoboController {
 
     }
 
-
-    @Transactional
+        @Transactional
     def save(Robo roboInstance) {
 
         selecaoCorrelacao()
@@ -63,68 +48,6 @@ class RoboController {
                 redirect roboInstance
             }
             '*' { respond roboInstance, [status: CREATED] }
-        }
-    }
-
-    def edit(Robo roboInstance) {
-        respond roboInstance
-    }
-
-    @Transactional
-    def update(Robo roboInstance) {
-        if (roboInstance == null) {
-            notFound()
-            return
-        }
-
-        if (roboInstance.hasErrors()) {
-            respond roboInstance.errors, view:'edit'
-            return
-        }
-
-        roboInstance.save flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'Robo.label', default: 'Robo'), roboInstance.id])
-                redirect roboInstance
-            }
-            '*'{ respond roboInstance, [status: OK] }
-        }
-    }
-
-    def ativarRobo(Robo roboInstance){
-
-        flash.message = "Robô "+roboInstance.nomeRobo.toString()+" ativado com sucesso!"
-                redirect action:"index"
-    }
-
-    @Transactional
-    def delete(Robo roboInstance) {
-
-        if (roboInstance == null) {
-            notFound()
-            return
-        }
-
-        roboInstance.delete flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Robo.label', default: 'Robo'), roboInstance.id])
-                redirect action:"index", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
-        }
-    }
-
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'roboInstance.label', default: 'Robo'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
         }
     }
     
